dfc06540816a7443fde176a290b02e26784aa496
pablorcruh <pablorcruh@gmail.com>
2024-10-14 21:58:56 -0500

    add service descovery

---
+++ eureka-server/src/test/java/ec/com/pablorcruh/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
